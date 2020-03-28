14350e9fcf3f069287a68c347f879a0eb977d625
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-28 11:01:26 +0800

    .

--- syntax_test3.R
+++ syntax_test3.R
@@ -1,3 +1,6 @@
 ######### TIDYR ##################
 ########################################################
 
+# import a built csv
+band_data_wide <- read.csv("band_data_wide.csv", stringsAsFactors = FALSE)
+View(band_data_wide)
