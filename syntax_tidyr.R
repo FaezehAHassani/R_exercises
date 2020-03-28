f3ad749a5e1a955dd696bc446b65692ef731fcca
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-28 16:58:50 +0800

    .

--- syntax_tidyr.R
+++ syntax_tidyr.R
@@ -15,3 +15,18 @@ band_data_long <- gather( # gather cells from band_data_wide data frame
 View(band_data_long)
 
 filter(band_data_long, band == "greensky_bluegrass") # to filter cells on greansky_bluegrass from the band_data_long data frame
+
+# reshape long data to wide data
+price_by_band <- spread(
+  band_data_long,
+  key = city,
+  value = price,
+)
+View(price_by_band)
+
+band_data_long_v2 <- unite(band_data_long, band, sep = "_", remove = TRUE, na.rm = FALSE) # return e.g. seattle_greensky_bluegrass_40 for each row
+View(band_data_long_v2)
+
+? unite # help on command "unite"
+
+
