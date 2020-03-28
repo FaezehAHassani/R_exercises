f07578e2c094edb480d850416dfc22bb97ff8b48
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-28 11:12:32 +0800

    .

--- syntax_test3.R
+++
@@ -1,6 +0,0 @@
-######### TIDYR ##################
-########################################################
-
-# import a built csv
-band_data_wide <- read.csv("band_data_wide.csv", stringsAsFactors = FALSE)
-View(band_data_wide)
---
+++ syntax_tidyr.R
@@ -0,0 +1,15 @@
+######### TIDYR ##################
+########################################################
+
+# import a built csv
+band_data_wide <- read.csv("band_data_wide.csv", stringsAsFactors = FALSE)
+View(band_data_wide)
+
+# convert wide format data frame to long format data frame
+band_data_long <- gather( # gather cells from band_data_wide data frame
+  band_data_wide,
+  key = band, # new column
+  value = price, # new column
+  -city # column that data from band_wide_wide are sorted based on, similar to select() in deplyr package
+)
+View(band_data_long)
