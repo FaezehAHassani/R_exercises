c078030f718ce13a96d40b8985338f3b8e3820f4
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-30 14:48:24 +0800

    update first tidyr file with first csv example

--- syntax_tidyr.R
+++ syntax_tidyr.R
@@ -7,10 +7,7 @@ library("dplyr")
 install.packages("tidyverse") # this package includes ggplot2 for visualisation
 library("tidyverse")
 
-install.packages("ggrepel") # to define geom_text_repel command
-library("ggrepel")
-
-# import a built csv
+########## import a built csv ##############
 band_data_wide <- read.csv("band_data_wide.csv", stringsAsFactors = FALSE)
 View(band_data_wide)
 
@@ -43,55 +40,3 @@ View(band_data_long_v3)
 
 ? separate
 
-#### load new csv file ####
-  
-wb_data <- read.csv(
-  "world_bank_data.csv",
-  stringsAsFactors = F
-  # skip = 3 to skip the first 5 rows of data
-  )
-View(wb_data)
-
-install.packages("naniar")
-library("naniar")
-
-indicator_of_interest <- "Government expenditure on education, US$ (millions)"
-expenditure_plot_data <- wb_data %>%
- rename(indicator = Series) %>%
- rename(X2010 = X2010..YR2010.) %>%
- rename(X2013 = X2013..YR2013.) %>%
- rename(X2014 = X2014..YR2014.) %>%
- rename(X2015 = X2015..YR2015.) %>%
- rename(X2016 = X2016..YR2016.) %>%
- rename(X2017 = X2017..YR2017.) %>%
- rename(X2018 = X2018..YR2018.) %>%
- filter(indicator == indicator_of_interest)
-
-# to replce ".." valuses in the data frame to NA
-expenditure_plot_data <- expenditure_plot_data %>%  
- replace_with_na(replace = list(X2010 = c("..")))
-
-View(expenditure_plot_data)
-
-# is.data.frame(expenditure_plot_data) return TRUE
-
-library('scales') # adding this library removed the error: Error in check_breaks_labels(breaks, labels) : object 'percent' not found
-
-expenditure_chart <- ggplot(data = expenditure_plot_data) +
- geom_point() + # add dots for data points
- geom_text_repel( # add text on data points
-  mapping = aes(x = X2010 / 100, y = X2018 / 100, label = Country.Code)) +
- scale_x_continuous(labels = percent) + scale_y_continuous(labels = percent) +
- labs(title = indicator_of_interest, x = "Expenditure 2010", y = "Expenditure 2018")
-
-
-
-# ? aes #aesthetic mapping
-# ? geom_tex
-# ? labels
-# ? scale_x_continuous
-# ? ggproto
-
-
-
- 
