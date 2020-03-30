6090e1b6fc5d2ee7302a8e2780c85c4a5f25d55e
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-30 15:32:47 +0800

    update syntax_tidyr_worlddatabank file_remove all NA values

--- syntax_tidyr_worlddatabank.R
+++ syntax_tidyr_worlddatabank.R
@@ -49,18 +49,31 @@ expenditure_plot_data <- expenditure_plot_data %>%
 
 View(expenditure_plot_data)
 
+# remove NA from data
+expenditure_plot_data_noNA <- expenditure_plot_data %>% 
+ drop_na(X2010) %>% 
+ drop_na(X2013) %>% 
+ drop_na(X2014) %>% 
+ drop_na(X2015) %>% 
+ drop_na(X2016) %>% 
+ drop_na(X2017) %>% 
+ drop_na(X2018)  
+  
+View(expenditure_plot_data_noNA)
+
 # plot
 expenditure_chart <- ggplot(data = expenditure_plot_data) +
- geom_point() + # add dots for data points
+ #geom_point() + # add dots for data points
  geom_text_repel( # add text on data points
   mapping = aes(x = X2010 / 100, y = X2018 / 100, label = Country.Code)
  ) +
  scale_x_continuous(labels = percent) + 
  scale_y_continuous(labels = percent) +
- labs(title = indicator_of_interest, x = "Expenditure 2010", y = "Expenditure 2018")
+ labs(title = indicator_of_interest, y = "Expenditure 2018", x = "Expenditure 2010")
 
 options(max.print = 99999) # when the number of points are alot it the show() command return the error of reaching mx.print, this command increase the maximum number
 show(expenditure_plot_data)
+
 
 # is.data.frame(expenditure_plot_data) return TRUE
 # ? aes #aesthetic mapping
