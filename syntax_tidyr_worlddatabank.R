62ad7973b5981c2609d1c632fe023d80e86520b9
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-03-31 11:48:26 +0800

    update syntax_tidyr_worlddatabank file_solve plot error

--- syntax_tidyr_worlddatabank.R
+++ syntax_tidyr_worlddatabank.R
@@ -51,37 +51,49 @@ View(expenditure_plot_data)
 
 # remove NA from data
 expenditure_plot_data_noNA <- expenditure_plot_data %>% 
- drop_na(X2010) %>% 
+ drop_na(X2010) %>%
  drop_na(X2013) %>% 
  drop_na(X2014) %>% 
  drop_na(X2015) %>% 
  drop_na(X2016) %>% 
  drop_na(X2017) %>% 
  drop_na(X2018)  
-  
+
+# all the X2010-2018 columns are in string format however they should be converted to numbers
+expenditure_plot_data_noNA$X2010 <- as.numeric(expenditure_plot_data_noNA$X2010)
+expenditure_plot_data_noNA$X2013 <- as.numeric(expenditure_plot_data_noNA$X2013)
+expenditure_plot_data_noNA$X2014 <- as.numeric(expenditure_plot_data_noNA$X2014)
+expenditure_plot_data_noNA$X2015 <- as.numeric(expenditure_plot_data_noNA$X2015)
+expenditure_plot_data_noNA$X2016 <- as.numeric(expenditure_plot_data_noNA$X2016)
+expenditure_plot_data_noNA$X2017 <- as.numeric(expenditure_plot_data_noNA$X2017)
+expenditure_plot_data_noNA$X2018 <- as.numeric(expenditure_plot_data_noNA$X2018)
+
+# check if the column are numbers now for example for X2018?
+expenditure_plot_data_noNA$X2018
+
 View(expenditure_plot_data_noNA)
 
-# plot
-expenditure_chart <- ggplot(data = expenditure_plot_data) +
- #geom_point() + # add dots for data points
- geom_text_repel( # add text on data points
-  mapping = aes(x = X2010 / 100, y = X2018 / 100, label = Country.Code)
- ) +
- scale_x_continuous(labels = percent) + 
- scale_y_continuous(labels = percent) +
- labs(title = indicator_of_interest, y = "Expenditure 2018", x = "Expenditure 2010")
-
 options(max.print = 99999) # when the number of points are alot it the show() command return the error of reaching mx.print, this command increase the maximum number
-show(expenditure_plot_data)
 
+show(expenditure_chart) # if the plot doesn't show up automatically you can use this command
+print(expenditure_chart) # if the plot doesn't show up automatically you can use this command
+
+# plotting with ggplot2 
+ggplot(data = expenditure_plot_data_noNA) +
+  geom_text_repel( # add text to data points
+    mapping = aes(x = X2010 / 100, y = X2018 /100, label = Country.Code), # define columns related to x and y
+  ) +
+  scale_x_continuous() + # scale_x_continuous(label = percent) will add % sign to the values on x_axis
+  scale_y_continuous() +
+  labs(title = indicator_of_interest, y = "Expenditure 2018", x = "Expenditure 2010")
 
 # is.data.frame(expenditure_plot_data) return TRUE
 # ? aes #aesthetic mapping
 # ? geom_tex
 # ? labels
-# ? scale_x_continuous
+#? scale_x_continuous
 # ? ggproto
-
+#? geom_text_repel
 
 
  
