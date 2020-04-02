c654989764f34e929dcc11e15183aa16910c0da3
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-02 11:17:41 +0800

    create syntax access database file

---
+++ syntax_access_database.R
@@ -0,0 +1,13 @@
+########### ACCESS DATABASE USING R #####################
+
+###### Install packages #############
+install.packages("dbplyr")
+library("DBI")
+library("dplyr")
+
+install.packages("RSQLite")
+library("RSQLite")
+
+install.packages("RPostgreSQL")
+library("RPostgreSQL")
+#################################
