baaddb0185e31b1bc3afd3a6eb5e7f751a6566a3
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-03 17:02:29 +0800

    update syntax file for accessing web API

--- syntax_access_webAPI.R
+++ syntax_access_webAPI.R
@@ -1,4 +1,10 @@
 ######## ACCESS WEB API ###########
 
+##### Install packages ###########
 install.packages("httr")
 library("httr")
+
+######## Get data from web API ############
+
+url <- "https://api.github.com/search/repositories?q=dplyr&sort=forks" # the URI to access data, this can be also typed in the web browser to see the data
+response <- GET(url) # return data using HTTP GET request
