1df5aff8b9a7ec2da6ed3f9bf202bf14b160a99b
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-04-29 15:48:18 +0800

    update syntax file for interactive visualisation

--- syntax_interactive_visual.R
+++ syntax_interactive_visual.R
@@ -1,13 +1,29 @@
 ########## Interactive visualisation in R ##########
 
-
-
-
+install.packages("plotly")
+library("plotly")
 
 ####### plotting iris built-in data set within ggplot2 #####
+View(iris)
 
+ggplot(data = iris) +
+  geom_point(mapping = aes(x = Sepal.Width, y = Petal.Width, color = Species))
 
+# use plot_ly to create an interactive plot from iris data => first plot with ggplot and then wrap it wioth ggplotly
+flowerplot <- ggplot(iris) +
+  geom_point(mapping = aes(x = Sepal.Width, y = Petal.Width, color = Species))
+ggplotly(flowerplot)
 
+# similar to the above, below codes with plot_ly can create a similar interactive plot
+
+plot_ly(
+  data = iris,
+  x = ~Sepal.Width,
+  y = ~Petal.Width,
+  color = Species,
+  type = "scatter",
+  mode = "markers"
+)
 
 
 
