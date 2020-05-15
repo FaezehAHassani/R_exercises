570ff1a4e2a70bb0b78c324208c942eaaf8cd46d
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-05-15 10:59:54 +0800

    update test_for_RMarkdon.R file to be called in RMarkdown_example.Rmd file

--- test_for_RMarkdown.R
+++ test_for_RMarkdown.R
@@ -1,5 +1,5 @@
 # I would like to add this R code file to my .Rmd file by using source() command
 
-fruits <- c("orange", "apple", "apricot", "bananas")
+fruits <- c("**orange**", "**apple**", "apricot", "bananas")
 
 
