ec4850b8cb7472f82c79856bb6891a51770cac8d
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-29 14:38:24 +0800

    update syntax file for mapping csv data with leaflet

--- shooting_leaflet.R
+++ shooting_leaflet.R
@@ -1,4 +1,11 @@
 # map of fatal police shooting using leaflet
+------------------------------------------
+  
+library("leaflet")
 
-librart("leaflet")
+------------------------------------------
 
+shootings <- read.csv("data/police_shootings.csv", stringsAsFactors = FALSE)
+View(shootings)
+
+palette_fn <- colorFactor(palette = "Dark2", domain = shootings[["race"]])
