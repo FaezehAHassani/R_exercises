bccba814e801b67a91259183ba3b8529128ccda4
FaezehAHassani <57852496+FaezehAHassani@users.noreply.github.com>
2020-06-29 14:42:34 +0800

    update syntax file for mapping csv data with leaflet

--- shooting_leaflet.R
+++ shooting_leaflet.R
@@ -9,3 +9,15 @@ shootings <- read.csv("data/police_shootings.csv", stringsAsFactors = FALSE)
 View(shootings)
 
 palette_fn <- colorFactor(palette = "Dark2", domain = shootings[["race"]])
+
+leaflet(data = shootings) %>%
+  addProviderTiles("Stamen.TonerLite") %>%
+  addCircleMarkers(
+    lat = ~lat,
+    lng = ~long,
+    label = ~paste0(name, ", ", age),
+    color = ~palette_fn(shootings[["race"]]),
+    fillOpacity = 0.7,
+    radius =  4,
+    stroke = FALSE
+  )
